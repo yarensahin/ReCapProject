1335567a8e795864242d85c95cedb305e12665e2
YAREN ŞAHİN <l5817021@std.yildiz.edu.tr>
2021-02-05 01:34:53 +0300

    Add project files.

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -10,11 +10,20 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            Car newCar = new Car
+            {
+                BrandId = 7,
+                ColorId = 18,
+                DailyPrice = 1800,
+                Description = "Toyota",
+                ModelYear = 2017,
+            };
             CarManager carManager = new CarManager(new EfCarDal());
             foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
-            }  
+            }
+            carManager.Add(newCar);
             
         }
     }
